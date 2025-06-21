c4f8dd029f450d88231109c9e17a9c055866a115
HiIamSanjay <sanjusumesh123@gmail.com>
2025-06-21 14:47:48 +0530

    made mobile app to stream video and the joystic to control the robot

--- welcome_robot_controller/lib/main.dart
+++ welcome_robot_controller/lib/main.dart
@@ -1,8 +1,9 @@
-import 'dart:html';
-import 'dart:ui' as ui;
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_mjpeg/flutter_mjpeg.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -29,26 +30,20 @@ class RobotControllerPage extends StatefulWidget {
 }
 
 class _RobotControllerPageState extends State<RobotControllerPage> {
-  final String videoFeedUrl =
-      'http://127.0.0.1:5000/video_feed?ts=${DateTime.now().millisecondsSinceEpoch}';
+  // Replace this with your PC’s actual IP address on your Wi-Fi network
+  final String serverIp = '192.168.1.3';
+
+  late final String videoFeedUrl;
+  late final String apiUrl;
 
   @override
   void initState() {
     super.initState();
-
-    // Register iframe for video stream
-    ui.platformViewRegistry.registerViewFactory(
-      'videoFeed',
-      (int viewId) => IFrameElement()
-        ..width = '100%'
-        ..height = '100%'
-        ..src = videoFeedUrl
-        ..style.border = 'none',
-    );
+    videoFeedUrl = 'http://$serverIp:5000/video_feed';
+    apiUrl = 'http://$serverIp:5000/move';
   }
 
   Future<void> sendDirection(String direction) async {
-    const String apiUrl = 'http://127.0.0.1:5000/move';
     try {
       final response = await http.post(
         Uri.parse(apiUrl),
@@ -76,7 +71,11 @@ class _RobotControllerPageState extends State<RobotControllerPage> {
             child: Container(
               margin: const EdgeInsets.all(10),
               decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-              child: const HtmlElementView(viewType: 'videoFeed'),
+              child: Mjpeg(
+                stream: videoFeedUrl,
+                fit: BoxFit.cover,
+                isLive: true,
+              ),
             ),
           ),
 
